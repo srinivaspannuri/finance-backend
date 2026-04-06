e8122a1b6089a9e6371d908d8692d8ca75c60e26
srinivaspannuri <srinivaspannuri0506@gmail.com>
2026-04-06 21:36:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,21 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"message": "Finance Backend Running"}
+
+@app.get("/users")
+def get_users():
+    return [
+        {"id": 1, "name": "John"},
+        {"id": 2, "name": "Alice"}
+    ]
+
+@app.get("/transactions")
+def get_transactions():
+    return [
+        {"id": 1, "amount": 1000, "type": "credit"},
+        {"id": 2, "amount": 500, "type": "debit"}
+    ]
+
+@app.get("/balance")
+def get_balance():
+    return {"balance": 5000}
